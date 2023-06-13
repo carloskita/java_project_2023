488574e1ac297f3d20d03ce9947ba39bebb326d8
carloskita <carloskitazawa@hotmail.com>
2023-06-13 19:38:02 -0300

    vai teia

--- churrasqueira/settings.py
+++ churrasqueira/settings.py
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/calendario'
 LOGOUT_REDIRECT_URL = '/home'
+
+CSRF_TRUSTED_ORIGINS = ['https://javaproject2023-production.up.railway.app/']
