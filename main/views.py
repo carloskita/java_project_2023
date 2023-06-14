fe48065abd857efdbe2587c9ad5f7900eff9c49a
carloskita <carloskitazawa@hotmail.com>
2023-06-14 18:01:34 -0300

    flamengo

--- main/views.py
+++ main/views.py
@@ -42,7 +42,6 @@ def cadastro(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
             return redirect('/calendario')
     else:
         form = RegisterForm()
